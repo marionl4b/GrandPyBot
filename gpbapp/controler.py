362f459ec1e4e8be3fb589ad3fe80b3fca759d2b
marionl4b <marion.labarre@gmail.com>
2019-02-15 15:17:03 +0100

    Refactor controler

--- gpbapp/controler.py
+++ gpbapp/controler.py
@@ -14,6 +14,23 @@ def index():
 
 @app.route('/index', methods=['POST'])
 def user_message():
+    def process_parser(data):
+        parsed_msg = parser.parse_usermsg(data)
+        if parsed_msg is None:
+            return jsonify({"usr_location": "error"})
+        else:
+            return process_gmap(parsed_msg)
+
+    def process_gmap(parsed_msg):
+        gmap_r = gmap.get_geocode(parsed_msg)
+        if gmap_r is None:
+            return jsonify({"usr_location": "error"})
+        gmap_results = gmap.parse_results(gmap_r)
+        if gmap_results is None:
+            return jsonify({"usr_location": "error"})
+        else:
+            return process_wiki(gmap_results)
+
     def process_wiki(gmap_results):
         wiki_results = wiki.prepare_data(gmap_results.get("search_term"))
         if wiki_results is None:
@@ -21,18 +38,6 @@ def user_message():
         else:
             return jsonify({"usr_location": "ok", "gmap": gmap_results, "wiki": wiki_results})
 
-    data = request.json
-    parsed_msg = parser.parse_usermsg(data)
+    usr_data = request.json
+    return process_parser(usr_data)
 
-    if parsed_msg is None:
-        return jsonify({"usr_location": "error"})
-    else:
-        gmap_r = gmap.get_geocode(parsed_msg)
-        if gmap_r is None:
-            return jsonify({"usr_location": "error"})
-
-        gmap_results = gmap.parse_results(gmap_r)
-        if gmap_results is None:
-            return jsonify({"usr_location": "error"})
-        else:
-            return process_wiki(gmap_results)
